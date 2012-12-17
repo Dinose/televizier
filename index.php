b7a069fa2ad92db2f22793cba5f751b8cce1fc40
Dennis Kramer <dennis@studiogewoon.nl>
2012-12-17 08:09:27 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,342 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href='css/my-style.css' />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,800,700' rel='stylesheet' type='text/css'>
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+
+<!-- Header -->
+
+        <header>
+             <div id="hoofdmenu">
+                 <div class="container">
+                    <div id="logo"><h1>De Gouden Televizier-Ring</h1></div>
+                    
+                    <div id="menu_pijl"></div>
+
+                    <nav id="menu_hoofdmenu">
+                        <ul>
+                            <li><a href="#introductie">Introductie</a></li>
+                            <li><a href="#print">De mogelijkheden</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </nav>    
+                 </div>
+             </div>
+
+             <div id="dropdown" >
+                 <nav id="menu_submenu" class="container">
+                     <ul>
+                        <li><a href="#">Print</a></li>
+                        <li><a href="#">Online</a></li>
+                        <li><a href="#">Social Media</a></li>
+                        <li><a href="#">Event</a></li>
+                     </ul>
+                 </nav>
+             </div>    
+         </header> 
+
+<!-- Leader -->
+
+         <section id="leader" class="leader_margin">
+            <hgroup>
+                <h1>Sterren Gezocht</h1>
+                <h2>Bent u de grote winnaar van de<br>
+                    <span>Gouden Televizier-Ring verkiezing 2013?</span>
+                </h2>
+            </hgroup>
+
+            <div id="leader_pijl"></div>
+            
+            <p>©RVDA</p>
+
+            <div id="leader_img"></div>             
+         </section> 
+
+<!-- Introductie -->
+
+         <section id="introductie" class="rij">
+             <div class="container">
+                 <h1>Word zélf de grote winnaar van de Gouden Televizier-Ring verkiezing 2013!</h1>
+                 <p class="inleiding">
+                    <b>Campagneperiode stemsite: 1 september 2013 – 1 oktober 2013. <br>
+                        Uitzending 18 oktober 2013.</b><br>
+                    <br>
+                    De Televizierlezers worden in het blad opgeroepen om te stemmen. Tevens worden diverse kanalen ingeschakeld, zoals televisie, om Nederland op te roepen om te stemmen. In 2012 heeft RTL Boulevard gedurende de hele campagne-periode aandacht besteed aan de genomineerden en de verkiezingen.
+                </p>
+
+                <p>
+                    <b>Mis het niet</b><br>
+                    Elk evenement heeft voorbereiding nodig. En dat geldt zeker voor de Gouden Televizier-Ring. Wij zijn razend enthousiast over het pakket aan mogelijkheden dat wij u nu al kunnen bieden. Het is onze rode loper. Speciaal voor u, uw bedrijf en uw boodschap. Dit mag u niet missen.
+                </p>
+
+                <p> 
+                    <b>Maatwerk</b><br>
+                    Maximale exposure, sterke branding. En sponsormogelijkheden genoeg. Van een enkele uiting tot een volledig geïntegreerde marketingcampagne. Meerdere mediakanalen staan tot uw beschikking. Elk met hun eigen kwaliteiten en doelgroepen. Zorg dat de uitreiking van de Gouden Televizier-Ring 2013 in het teken staat van úw sponsorboodschap! Wij leveren het maatwerk wel.
+                </p>
+
+                <figure>
+                    <img src="img/boulevard.png" alt="De Gouden Televizier-Ring op RTL Boulevard" width="600p" />
+                    <figcaption>
+                        Gedurende de campagne-periode besteed <b>RTL Boulevard</b> veel aandacht aan de genomineerden en de verkiezingen
+                    </figcaption>
+                </figure> 
+
+                <div id="instructie">
+                    <p>
+                        <span>Bekijk de mogelijkheden per kanaal</span><br>
+                        <b>Scroll</b> naar beneden of <b>gebruik je pijltjestoetsen</b>
+                    </p>
+                    <a href="#"></a>
+                </div>
+
+             </div>
+         </section>
+
+<!-- De mogelijkheden -->
+
+        <section id="leader">
+            <p>©RVDA</p>
+            <div id="goudeloper_img"></div>             
+         </section> 
+
+         <div id="sidenav">
+             <ul>
+                 <li><a href="">&#9679</a></li>
+                 <li><a href="">&#9679</a></li>
+                 <li><a href="">&#9679</a></li>
+                 <li><a href="">&#9679</a></li>
+             </ul>
+
+         </div>
+
+
+    <!-- Print -->
+
+            <section id="print" class="mogelijkheid rij wit">
+                <div class="container">
+                    <h1>Print</h1>
+                    <p class="inleiding">
+                        In aanloop naar de uitreiking van de Gouden Televizier-Ring 2013 staat onze titel Televizier helemaal tot uw beschikking. Of het nu gaat om aantrekkelijke advertentiepagina’s of advertorials met meerwaarde, wij denken graag met u mee. Natuurlijk kunt u ook bij onze activiteiten aansluiten die wij samen met andere printtitels ondernemen. <b>Maximaal bereik, maximaal voordeel</b>. 
+                    </p>
+
+                    <div class="clearfix"></div>
+
+                    <figure>
+                        <img src="img/tijdschriften.png" alt="Adverteren in de Televizier, Avrobod en TVFILM" width="281" /> 
+                    </figure> 
+
+                    <div class="mogelijkhedenlijst">
+                        <h3>De mogelijkheden</h3>
+                        <ul>
+                            <li>Het merk wordt in communicatie-uitingen genoemd die Televizier inzet in 2013</li>
+                            <li>4x vermelding van de sponsor op Televizier-outsert (oproep tot stemmen)</li>
+                            <li>4x vermelding van de sponsor in de advertentie van Televizier</li>
+                            <li>5x 1/1 pagina in Televizier</li>
+                            <li>5x 1/1 pagina in Avrobode</li>
+                            <li>2x 1/1 pagina in TVFilm</li>
+                        </ul>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                </div>
+            </section>
+     
+    <!-- Online -->
+
+            <section id="online" class="mogelijkheid rij grijs">
+                <div class="container">
+                    <h1>Online</h1>
+                    <p class="inleiding">
+                        Televizier.nl is dé plek voor het laatste nieuws over tv.  Op deze site komt u in een positief umfeld in contact met consumenten die álles willen weten over tv! 
+                    </p>
+
+                    <div id="screenshots">
+                        <img src="img/laptop.png" alt="De Televizier-Ring online" width="309"/> 
+                    </div> 
+
+                    <div class="clearfix"></div>
+
+                    <section id="televizier">
+                        <h1>Televizier.nl</h1>
+                        <p>
+                            Televizier richt zich op de Nederlandstalige programma’s waar iedereen graag over meepraat: amusement, series, soaps en films. Televizier.nl gaat verder waar het blad stopt en geeft elke dag het laatste nieuws over de wereld van televisie en de sterren, met veel videomateriaal en een uitgebreid archief over de populairste programma’s. 
+                        </p>
+                        <p>
+                            Daarnaast plaatst Televizier.nl veel dvd-reviews, vindt u hier de columns van Peter van der Vorst, leuke prijsvragen en een uitgebreide tv-gids. Bezoekers en lezers kunnen op Televizier.nl hun mening geven over tv. In september is Televizier.nl de site waar de hoofdverkiezing van de Televizier Ring van start gaat: dé televisieprijs van Nederland. Rond die tijd zorgt de verkiezing voor extra hoge bezoekcijfers op Televizier.nl. 
+                        </p>
+                        <p>
+                            <b>Doelgroep</b><br>
+                            Televizier spreekt met haar site mannen en vrouwen (50/50) aan in de leeftijd van 25-39 jaar. Ze houden van amuserende televisie en zijn online actief.
+                        </p> 
+                    </section>
+
+                    <section id="televizier-ring">
+                        <h1>Televizier.nl/ring</h1>
+                        <p>
+                            Via de stemtool op Televizier.nl kan er gestemd worden op de kanshebbers voor de Gouden Televizier-Ring, de Zilveren Televizier-Sterren (voor tv-man en tv-vrouw van het jaar), de Televizier Talent Award en de Gouden Stuiver (beste kinderprogramma). Voor de Gouden Televizier-Ring worden twee stemrondes gehouden, voor de overige categorieën één temronde. Er kan maximaal één stem per categorie uitgebracht worden. Het is niet mogelijk om vaker te stemmen. 
+                        </p>
+                        <p>
+                            <b>Doelgroep*</b><br>
+                            De doelgroep van de Televizier-Ringverkiezing is ‘gemiddeld 
+                            Nederland’. En bij een profielonderzoek naar de stemmers komen de volgende oververtegenwoordigingen naar voren: 
+                            De stemmers komen veelal uit de randstedelijke gebieden, ze hebben veelal hogere inkomens en een hoge welstandsklasse. Er wordt vaker gestemd door vrouwen dan door mannen. 
+                        </p> 
+                    </section>
+
+                    <div class="clearfix"></div>
+
+                    <div class="mogelijkhedenlijst">
+                        <h3>De mogelijkheden</h3>
+                        <ul>
+                            <li>Homepage Take Over op Televizier.nl</li>
+                            <li>Homepage Take Over op Ringpagina</li>
+                            <li>Banner op subpagina's Ring</li>
+                            <li>Interstitial op homepage Ringpagina</li>
+                            <li>Aanbieding na stemprocedure</li>
+                            <li>Banner op alle pagina's Ringverkiezing (homepage en subpagina's)</li>
+                        </ul>
+                    </div>
+
+                    <blockquote>
+                        <h1>In 2012 zijn er bijna <span>500.000 stemmen</span> uitgebracht! </h1>
+                        <p>* Bron: Cendris profiel onderzoek Televizier stemmers 2010.</p> 
+                    </blockquote>
+
+                    <div class="clearfix"></div>
+
+                    <div id="bereik">
+                        <h3>Bereik gedurende stemperiode <span>( 6-8weken )</span></h3>
+                        <ul>
+                            <li>800.000<br>     <span>Unieke bezoekers</span></li>
+                            <li>2.000.000<br>   <span>Pageviews binnen de Ring pagina's</span></li>
+                            <li>1.300.000<br>   <span>Pageviews op de homepage Ring-pagina</span></li>
+                        </ul>
+                    </div>
+
+
+                </div>
+            </section>
+      
+    <!-- Social Media -->
+
+            <section id="socialmedia" class="mogelijkheid rij wit">
+                <div class="container">
+                    <h1>Social Media</h1>
+                    <p class="inleiding">
+                        In 2012 is er bij de Televizier-Ring verkiezing veelvuldig gebruikgemaakt van de verschillende Social Media kanalen. Zo was het gemakkelijk om je stem te delen met je vrienden via Facebook, Twitter en Hyves en hier een persoonlijke boodschap bij te plaatsen.  
+                    </p>
+
+                    <div class="mogelijkhedenlijst">
+                        <h3>De mogelijkheden</h3>
+                        <ul>
+                            <li>Vermelding op Facebook + eindscherm Facebook-game (Facebook app)</li>
+                            <li>Twitterpagina takeover, gedurende hele stemperiode</li>
+                            <li>‘Mede mogelijk gemaakt door’-vermelding na delen door stemmers</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
+    <!-- Event --> 
+
+            <section id="event" class="mogelijkheid rij grijs">
+                <div class="container">
+                    <h1>Event</h1>
+
+                    <div class="mogelijkhedenlijst">
+                        <h3>De mogelijkheden</h3>
+                        <ul>
+                            <li>Award-vernoeming door klant</li>
+                            <li><span style="display:inline-block; text-indent:0px;">Plek op wallpaper op event Diverse printmedia (Story en glossy magazines) besteden achteraf aandacht aan de sterren op de rode loper. Uw merk krijgt dus achteraf nog veel aandacht.</span></li>
+                            <li>Aftiteling</li>
+                            <li>Goodiebag</li>
+                            <li>Vermelding in de nieuwsbrieven</li>
+                            <li>Meelopen in pr-aandacht: aankondiging en verslag</li>
+                            <li>Product placement: sponsoring van bar, zaal</li>
+                            <li>Exclusieve toegangskaarten</li>
+
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
+        </section>
+
+
+<!-- Contact -->
+
+        <section id="contact">
+            <div class="container">
+                <img src="img/footer_logo.png" alt="De Gouden Televizier-Ring">
+                <div id="tekst">
+                    <p>
+                        Wilt u eens praten over de mogelijkheden of heeft u ideeën? Laat het ons weten. U kunt contact opnemen met <a href="mailto:gebruiker@provider.nl?SUBJECT=Adverteren met de Gouden Televizier-Ring">Rob Lucassen</a> of bellen met <b>035 - 672 6903</b>.
+                    </p>
+                    <ul>
+                        <li>
+                            <a id="twitter" href="https://twitter.com/TelevizierRing" target="_blank">
+                              
+                            </a>
+                        </li>
+                        <li>
+                            <a id="facebook" href="https://www.facebook.com/pages/Gouden-Televizier-Ring/115637391858731" target="_blank">
+                               
+                            </a>
+                        </li>
+                        <li>
+                            <a id="mail" href="mailto:gebruiker@provider.nl?SUBJECT=Adverteren met de Gouden Televizier-Ring">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="clearfix"></div>
+
+            </div>
+        </section>
+
+<!-- Footer -->
+
+        <footer>
+            <div class="container">
+                <p>Televizier-Ring ©2012/2013 Alle rechten voorbehouden. Beelden niet rechtenvrij te gebruiken.</p>
+            </div>
+        </footer>
+
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
